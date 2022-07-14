fe6a119eb9f9c99a00e688d0de0649e0886a382e
Николай Слесарев <kolansers@yandex.ru>
2022-07-14 23:36:49 +0300

    Add own exceptions. Removed flaws

--- homework.py
+++ homework.py
@@ -8,6 +8,8 @@ from os import getenv
 
 from dotenv import load_dotenv
 
+from exceptions import EmptyDictInResponseError, JSONError, StatusCodeError
+
 load_dotenv()
 
 
@@ -43,9 +45,9 @@ def send_message(bot, message):
     """
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logger.info(f'Успешная отправка сообщения в Telegram: {message}')
+        logger.info('Успешная отправка сообщения в Telegram: %s', message)
     except Exception as error:
-        logger.error(f'Сбой при отправке сообщения в Telegram: {error}')
+        logger.error('Сбой при отправке сообщения в Telegram: %s', error)
 
 
 def get_api_answer(current_timestamp):
@@ -54,18 +56,34 @@ def get_api_answer(current_timestamp):
     В случае успешного запроса должна вернуть ответ API, преобразовав
     его из формата JSON к типам данных Python.
     """
+    logger.info('Начало запроса к API')
     timestamp = current_timestamp
     params = {'from_date': timestamp}
-    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     try:
-        homeworks = requests.get(ENDPOINT, headers=headers, params=params)
+        homeworks = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    except requests.ConnectionError as error:
+        logger.error('Произошла ошибка подключения: %s', error)
+    except requests.URLRequired as error:
+        logger.error('Ошибка в URL-адресе "%s": %s', homeworks.url, error)
     except Exception as error:
-        logger.error(f'Ошибка при запросе к API Практикум.Домашка: {error}')
+        logger.error('Ошибка при запросе к API Практикум.Домашка: %s', error)
     if homeworks.status_code != HTTPStatus.OK:
-        raise Exception(
+        raise StatusCodeError(
             f'Код HTTP ответа при запросе к API - {homeworks.status_code}'
+            f' Headers при запросе к API - "{homeworks.headers}"'
         )
-    return homeworks.json()
+    try:
+        homeworks_json = homeworks.json()
+    except Exception as error:
+        raise JSONError(
+            (
+                'Ошибка при преобразовании ответа из'
+                f' JSON к типам данных Python: {error}'
+            )
+        )
+    else:
+        logger.info('Запроса к API осуществлен успешно')
+        return homeworks_json
 
 
 def check_response(response):
@@ -75,23 +93,22 @@ def check_response(response):
     должна вернуть список домашних работ (он может быть и пустым),
     доступный в ответе API по ключу 'homeworks'.
     """
-    if response == {}:
-        logger.error('Ответ от API содержит пустой словарь')
-        raise Exception('Ответ от API содержит пустой словарь')
+    logger.info('Проверка ответа API на корректность')
+    if not response:
+        raise EmptyDictInResponseError('Ответ от API содержит пустой словарь')
     if not isinstance(response, dict):
-        logger.error('Ответ от API не является словарем')
         raise TypeError('Ответ от API не является словарем')
+    if response.get('current_date') is None:
+        raise KeyError('Ответ от API не содержит ключ "current_date"')
     homeworks = response.get('homeworks')
-    if homeworks is not None:
-        if isinstance(homeworks, list):
-            return homeworks
-        else:
-            logger.error(
-                'Данные в ответе API по ключу "homeworks" не являются словарем'
-            )
-    else:
-        logger.error('Ответ от API не содержит ключ "homeworks"')
+    if homeworks is None:
         raise KeyError('Ответ от API не содержит ключ "homeworks"')
+    if not isinstance(homeworks, list):
+        raise TypeError(
+            'Данные в ответе API по ключу "homeworks" не являются словарем'
+        )
+    logger.info('Ответ API корректен')
+    return homeworks
 
 
 def parse_status(homework):
@@ -105,22 +122,16 @@ def parse_status(homework):
     homework_status = homework.get('status')
     verdict = HOMEWORK_STATUSES.get(homework_status)
     if homework_name is None:
-        logger.error(
-            'Словарь с домашней работой не содержит ключ "homework_name"'
-        )
         raise KeyError(
             'Словарь с домашней работой не содержит ключ "homework_name"'
         )
     if homework_status is None:
-        logger.error('Словарь с домашней работой не содержит ключ "status"')
         raise KeyError(
             'Словарь с домашней работой не содержит ключ "status"'
         )
     if verdict is None:
-        logger.error(
-            'Недокументированный статус домашней работы'
-        )
         raise KeyError('Недокументированный статус домашней работы')
+    logger.info('Статус проверки работы получен')
     return (
         f'Изменился статус проверки работы "{homework_name}". {verdict}'
     )
@@ -134,27 +145,29 @@ def check_tokens():
 def main():
     """Основная логика работы бота."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = 0
-    if check_tokens():
-        while True:
-            try:
-                response = get_api_answer(current_timestamp)
-                homeworks = check_response(response)
-                if len(homeworks):
-                    message = parse_status(homeworks[0])
-                    send_message(bot, message)
-                else:
-                    logger.debug('В ответе отстутвуют новые статусы проверки')
-                current_timestamp = response.get('current_date')
-                time.sleep(RETRY_TIME)
-            except Exception as error:
-                message = f'Сбой в работе программы: {error}'
-                send_message(bot, message)
-                time.sleep(RETRY_TIME)
-    else:
+    current_timestamp = int(time.time())
+    if not check_tokens():
         message = 'Отсутвует (нет доступа) к переменной окружения'
         logger.critical(message)
         send_message(bot, message)
+        sys.exit(message)
+    while True:
+        try:
+            response = get_api_answer(current_timestamp)
+            current_timestamp = response.get('current_date')
+            homeworks = check_response(response)
+            if homeworks:
+                message = parse_status(homeworks[0])
+                send_message(bot, message)
+            else:
+                logger.debug('В ответе отстутвуют новые статусы проверки')
+        except Exception as error:
+            logger.error(error)
+            message = f'Сбой в работе программы: {error}'
+            send_message(bot, message)
+        finally:
+            logger.info('Переход в режим ожидания: %d с', RETRY_TIME)
+            time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
